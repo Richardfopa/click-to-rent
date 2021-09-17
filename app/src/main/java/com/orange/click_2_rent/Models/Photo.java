4636abbbb3691a70f76fc901bdfb793e4fe1aefc
richardfp <richardfopa4@gmail.com>
2021-09-17 14:05:46 +0100

    Ajout du model de classe

---
+++ app/src/main/java/com/orange/click_2_rent/Models/Photo.java
@@ -0,0 +1,62 @@
+package com.orange.click_2_rent.Models; /***********************************************************************
+ * Module:  Photo.java
+ * Author:  Mannschaft
+ * Purpose: Defines the Class Photo
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid bb98ae54-8819-4753-a7a9-a40f93f86c0b */
+public class Photo {
+   /** @pdOid f3962377-27f1-40e4-8d7c-3b5bc74035a3 */
+   private int idPhoto;
+   /** @pdOid bf5fadae-969a-453b-8b49-966c5c3c51ee */
+   private String nomPhoto;
+
+   public int getIdPhoto() {
+      return idPhoto;
+   }
+
+   public Photo(int idPhoto, String nomPhoto) {
+      this.idPhoto = idPhoto;
+      this.nomPhoto = nomPhoto;
+   }
+
+   public Photo() {
+   }
+
+   public void setIdPhoto(int idPhoto) {
+      this.idPhoto = idPhoto;
+   }
+
+   public String getNomPhoto() {
+      return nomPhoto;
+   }
+
+   public void setNomPhoto(String nomPhoto) {
+      this.nomPhoto = nomPhoto;
+   }
+
+   /** @pdOid 50b6272a-7375-47f3-8337-643d8cc40086 */
+   public void modifierphoto() {
+      // TODO: implement
+   }
+   
+   /** @pdOid dfaaecc8-ab4e-4e05-be97-efd3272448bc */
+   public void ajouterPhoto() {
+      // TODO: implement
+   }
+   
+   /** @pdOid 6e9710a4-adf9-4bc4-917c-e243aeaaa2da */
+   public void supprimerPhoto() {
+      // TODO: implement
+   }
+
+   @Override
+   public String toString() {
+      return "Photo{" +
+              "idPhoto=" + idPhoto +
+              ", nomPhoto='" + nomPhoto + '\'' +
+              '}';
+   }
+}
---
+++ app/src/main/java/com/orange/click_2_rent/Models/Validation.java
@@ -0,0 +1,21 @@
+package com.orange.click_2_rent.Models; /***********************************************************************
+ * Module:  Validation.java
+ * Author:  Mannschaft
+ * Purpose: Defines the Class Validation
+ ***********************************************************************/
+
+import java.sql.Timestamp;
+import java.util.*;
+
+/** @pdOid 7fc37e9f-6807-447b-82d2-9e6da26091df */
+public class Validation {
+   /** @pdOid e66d20dd-565b-42cf-9420-6513ac9cb5fc */
+   private Timestamp date;
+
+   @Override
+   public String toString() {
+      return "Validation{" +
+              "date=" + date +
+              '}';
+   }
+}
