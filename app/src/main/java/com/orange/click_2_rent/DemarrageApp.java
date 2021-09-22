03e68ba37cd2f64231337c6a4a63f68c5fbdfaa8
Dalois30 <claudetapi@yahoo.com>
2021-09-22 04:33:49 +0100

    ajout de l'authentification anonyme et fin de ajout service

---
+++ app/src/main/java/com/orange/click_2_rent/DemarrageApp.java
@@ -0,0 +1,68 @@
+package com.orange.click_2_rent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DemarrageApp extends AppCompatActivity {
+
+    private static final String TAG = "debug";
+    private FirebaseAuth mAuth;
+    private Button btnStrat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demarrage_app);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        btnStrat = findViewById(R.id.btStart);
+        btnStrat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInAnonymously()
+                        .addOnCompleteListener(DemarrageApp.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInAnonymously:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(DemarrageApp.this, "Authentication success.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInAnonymously:failure", task.getException());
+                                    Toast.makeText(DemarrageApp.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+
+    void updateUI(FirebaseUser user){
+        if (user != null){
+            Intent i = new Intent(DemarrageApp.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+}
