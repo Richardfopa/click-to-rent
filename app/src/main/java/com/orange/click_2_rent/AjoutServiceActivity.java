9aba5e6b9220198dfa43d176d13d733fe9301322
jnr150771 <jnralene@gmail.com>
2021-10-06 18:10:29 +0100

    My Push

---
+++ app/src/main/java/com/orange/click_2_rent/AjoutServiceActivity.java
@@ -0,0 +1,458 @@
+package com.orange.click_2_rent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.orange.click_2_rent.Models.FirebasesUtil;
+import com.orange.click_2_rent.Models.Photo;
+import com.orange.click_2_rent.Models.Service;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
+public class AjoutServiceActivity
+        extends AppCompatActivity
+        implements View.OnClickListener, View.OnFocusChangeListener {
+
+    private FirebaseStorage storage;
+    private TextInputLayout mTxtTitleService;
+    private TextInputLayout mTxtCategorieService;
+    private ImageView mImgPhotoService;
+    private Button mbtChoosePict;
+    private ImageView mImgPhotoDoc;
+    private Button mbtChooseDoc;
+    private TextView mtxtphoto;
+    private TextView mtxtdoc;
+    private Button mbtcancel;
+    private Button mbtConfirm;
+    private StorageMetadata metadata;
+    private AutoCompleteTextView mautoComplete;
+    private TextView error;
+    private String title;
+    private String categorie;
+    private String mnomphoto;
+    private Uri uriphotoservice;
+    private String mnomdocument;
+    private Uri uridocservice;
+    private String description;
+    private ProgressBar mProgressBar;
+    private StorageReference mStorageRef;
+    private StorageReference mStorageRefImage;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
+
+    Service service = new Service();
+
+    //    private FirebaseStorage storage;
+    private ArrayList<Photo> listphoto = new ArrayList<>();
+    private TextInputLayout mTxtDescription;
+    private static final int REQUEST_SELECT_IMAGE_SERVICE = 10004;
+    private static final int REQUEST_SELECT_DOC_SERVICE = 10005;
+    private static final String COLLECTION_SERVICE = "services";
+
+    private UUID Uuid;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_service);
+
+        // Initialisation des variable de la vue
+        setVariablefromLayout();
+        // Gestion des click sur la vue
+        //Bouton confirmer
+        mbtConfirm.setOnClickListener(this);
+        //Bouton Annuler
+        mbtcancel.setOnClickListener(this);
+        //Bouton Choisir photo service
+        //mbtChoosePict.setOnClickListener(this);
+        //Bouton choisir doc service
+        //mbtChooseDoc.setOnClickListener(this);
+
+        mImgPhotoService.setOnClickListener(this);
+
+        mImgPhotoDoc.setOnClickListener(this);
+
+        mTxtTitleService.setOnFocusChangeListener(this);
+
+        // Configuration de firebasestorage
+
+        storage = FirebaseStorage.getInstance();
+        //  Create a storage reference from our app
+        mStorageRef = storage.getReference();
+
+        //  Create a sotarage reference to images
+
+        mStorageRefImage = mStorageRef.child("services");
+
+
+
+
+
+    }
+
+    private void setVariablefromLayout() {
+        String[] type = new String[] {"Batiment", "Electricite", "Hotellerie", "Covoiturage","Immobiler"};
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(
+                        this,
+                        R.layout.category_item,
+                        type);
+
+        mautoComplete =
+                findViewById(R.id.category);
+        mautoComplete.setAdapter(adapter);
+        mTxtTitleService = findViewById(R.id.txt_title_service);
+        mTxtCategorieService = findViewById(R.id.txt_category_service);
+        mTxtDescription = findViewById(R.id.tv_description);
+        mImgPhotoService = findViewById(R.id.image_add_service_photo_item);
+        mImgPhotoService.setMaxWidth(R.dimen.size_img_upload);
+        mImgPhotoService.setMaxHeight(R.dimen.size_img_upload);
+
+        mImgPhotoDoc = findViewById(R.id.image_add_service_document_item);
+        mImgPhotoDoc.setMaxHeight(R.dimen.size_img_upload);
+        mImgPhotoDoc.setMaxWidth(R.dimen.size_img_upload);
+        mtxtphoto = findViewById(R.id.textphoto_add_service);
+        mtxtdoc = findViewById(R.id.textdoc_add_service);
+        mbtcancel = findViewById(R.id.cancel_add_service);
+        mbtConfirm = findViewById(R.id.confirm_add_service);
+        error = findViewById(R.id.title_error_add_service);
+        uridocservice = null;
+        uriphotoservice = null;
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.confirm_add_service:
+
+                // Clic pour confirmer les donnees saisir et envoyer
+
+                title = mTxtTitleService.getEditText().getText().toString();
+                categorie = mTxtCategorieService.getEditText().getText().toString();
+                description = mTxtDescription.getEditText().getText().toString();
+
+                error.setText("Donnees Valider ");
+                error.setTextColor(R.color.theme_color);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    error.setCompoundDrawablesRelative(null,null,getDrawable(R.drawable.ic_baseline_check_24),null);
+                }
+
+                Log.d("NOERROR","pas erreur");
+
+                // Create a storage reference from our app
+
+                StorageReference storageRef = storage.getReference("services");
+
+                Uuid =  UUID.randomUUID();
+                StorageReference photoserviceRef = mStorageRef.child("services/photo"+title+Uuid);
+                StorageReference documentserviceRef = mStorageRef.child("services/doc"+title+Uuid);
+
+                // Get the data from an ImageView as bytes
+
+                uploadImageViewToStorage(mImgPhotoDoc,photoserviceRef);
+                // uploadDocToStorage(mImgPhotoService,documentserviceRef);
+                Toast.makeText(this, "Insertion reussi avec success ", Toast.LENGTH_SHORT).show();;
+                startActivity(new Intent(this,PrestationsActivity.class));
+
+                break;
+
+            case R.id.image_add_service_photo_item:
+                ChooseDoc(REQUEST_SELECT_IMAGE_SERVICE);
+                break;
+
+            case R.id.image_add_service_document_item:
+                ChooseDoc(REQUEST_SELECT_DOC_SERVICE);
+                break;
+
+            case R.id.cancel_add_service:
+                // Clic sur le bouton annuler
+
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + view.getId());
+        }
+    }
+
+    private void ChooseDoc(int requestSelectImageService) {
+
+        Intent intent = new Intent();
+        String[] type = {"image/png", "image/jpeg", "image/jpg", "image/gif"};
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, type);
+        }
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent,"Select your profile"), requestSelectImageService);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_SELECT_IMAGE_SERVICE && resultCode == RESULT_OK) {
+            Picasso
+                    .with(this)
+                    .load(data.getData())
+                    .fit()
+                    .centerCrop()
+                    .into(mImgPhotoService);
+            //setImage(mtxtphoto,mImgPhotoService,data);
+        }
+        if (requestCode == REQUEST_SELECT_DOC_SERVICE && resultCode == RESULT_OK) {
+            Picasso
+                    .with(this)
+                    .load(data.getData())
+                    .fit()
+                    .centerCrop()
+                    .into(mImgPhotoDoc)
+            ;
+        }
+
+    }
+
+
+    private String getPath(Uri uri) {
+
+        if( uri == null ) {
+            return null;
+        }
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
+
+    private void uploadFile() {
+
+    }
+
+    public void uploadImageViewToStorage(@NonNull ImageView imageview, @NonNull StorageReference photoRef){
+        imageview.setDrawingCacheEnabled(true);
+        imageview.buildDrawingCache();
+
+        Bitmap bitmap = ((BitmapDrawable) imageview.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = photoRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Log.d("STORAGE","FAILLURE INSERTION");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+                Log.d("STORAGE","INSERTION REUSSI");
+            }
+        });
+
+        // image imagedownload uri from firebase
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(
+                new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+
+                        // Continue with the task to get the download URL
+                        return photoRef.getDownloadUrl();
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri photoservice = task.getResult();
+                            TextView txt = findViewById(R.id.textphoto_add_service);
+                            txt.setText(photoservice.toString());
+                            Log.d("FILEURL", "onComplete: "+ photoservice.toString());
+                            listphoto.add(new Photo(Uuid.toString(),photoservice.toString(),photoservice.toString()));
+                            service.setAddDate(new Timestamp(new Date()));
+                            service.setId(""+Uuid);
+                            service.setCategorie(categorie);
+                            service.setPhotoService(photoservice.toString());
+                            service.setClients(null);
+                            service.setPhotos(listphoto);
+                            service.setCommentaire(null);
+                            service.setDescription(description);
+                            service.setName_provider("Gambee");
+                            service.setNote(null);
+                            service.setTitle(title);
+
+                            FirebasesUtil.addService(service);
+
+                        } else {
+
+                        }
+                    }
+                });
+
+    }
+    public void uploadDocToStorage(@NonNull ImageView imageview, @NonNull StorageReference photoRef){
+        imageview.setDrawingCacheEnabled(true);
+        imageview.buildDrawingCache();
+
+        Bitmap bitmap = ((BitmapDrawable) imageview.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = photoRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+                Log.d("STORAGE","INSERTION REUSSI");
+            }
+        });
+
+        // image imagedownload uri from firebase
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(
+                new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+
+                        // Continue with the task to get the download URL
+                        return photoRef.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    uridocservice = task.getResult();
+                    TextView txt = findViewById(R.id.textdoc_add_service);
+                    txt.setText(uridocservice.toString());
+                    Log.d("FILEURL", "onComplete: "+ uridocservice.toString());
+                } else {
+                    // Handle failures
+                    // ...
+                }
+            }
+        });
+
+    }
+
+
+    public void uploadtoStorage(Uri filephotoservice){
+
+        UploadTask uploadTask = mStorageRef.child("services/" + filephotoservice.getLastPathSegment()).putFile(filephotoservice, metadata);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                Log.d("STORAGE", "Upload is " + progress + "% done");
+            }
+        }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.d("STORAGE", "Upload is paused");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Log.w("STORAGE", "Upload is cancel",exception);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Handle successful uploads on complete
+                // ...
+                Log.d("STORAGE", "Upload is Success");
+            }
+        });
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasfocus) {
+        switch(view.getId()){
+            case R.id.txt_title_service:
+                if(hasfocus){
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (mTxtTitleService.getEditText().toString().length() < 4){
+                            mTxtTitleService.setBoxStrokeColor(getColor(R.color.rouge));
+                        }else {
+                            mTxtTitleService.setBoxStrokeColor(getColor(R.color.theme_color));
+                            mTxtTitleService.setEndIconDrawable(getDrawable(R.drawable.ic_baseline_check_24));
+                        }
+                    }
+                }
+                break;
+        }
+    }
+}
